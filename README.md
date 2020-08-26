30dc30baa12fcb0d8082df259ab51963d4603e37
huanhuan18 <848368042@qq.com>
2020-08-27 00:10:15 +0800

    前端搭建

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Django_web
 Based on Django and vue.js Developed web automation platform
 
-# 环境
-Django==2.2.15
-node==v12.18.3
+## 环境
+* Django==2.2.15
+* node==v12.18.3
+* mysql==5.7
 
-# 功能
+## 功能
 * 前端：vue
 * 后端：django
 * 自动化web平台项目，前后端分离
--- automation_web/automation_web/settings.py
+++ automation_web/automation_web/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home_page',
+    'test_page',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'automation_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_automation',
+        'USER': 'root',
+        'PASSWORD': '199517',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
